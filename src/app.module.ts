d5406fef6351ec0878190809d40e4e94900bce48
Bit-Barron <1bit.baron@gmail.com>
2023-01-06 21:09:43 +0100

    user.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { UsersModule } from './users/users.module';
 import { ReportsController } from './reports/reports.controller';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from 'src/reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.sqlite',
-      entities: [],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
