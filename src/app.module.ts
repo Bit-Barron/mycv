31a64e051e92a178b988abe53057ab5e0f4fd045
Bit-Barron <1bit.baron@gmail.com>
2023-01-06 20:11:30 +0100

    set up all

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './reports/users/users.module';
+import { ReportsController } from './reports/reports.controller';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, ReportsModule],
+  controllers: [AppController, ReportsController],
   providers: [AppService],
 })
 export class AppModule {}
