c863bddbfe64f3468c3cd66d1bb6ff482c3c1710
Bit-Barron <1bit.baron@gmail.com>
2023-01-07 03:06:15 +0100

    post

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,6 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
-    return this.repo.save(user);
+    return this.repo.save({ email, password });
   }
 }
