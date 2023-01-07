209d9e2c810b57bf27e6955f20c236bf35260b64
Bit-Barron <1bit.baron@gmail.com>
2023-01-07 03:15:20 +0100

    just start fixing typeeror in user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,16 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
-    return this.repo.save({ email, password });
+    return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find() {}
+
+  update() {}
+
+  remove() {}
 }
