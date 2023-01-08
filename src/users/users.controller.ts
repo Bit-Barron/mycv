e50fda1a54a315c5d4af418243a2ea1a4ea476b9
Bit-Barron <1bit.baron@gmail.com>
2023-01-08 18:44:08 +0100

    create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,6 +24,16 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session);
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Get('/colors/:color')
   setColor(@Param('color') color: string, @Session() session: any) {
     console.log(color);
@@ -39,7 +49,7 @@ export class UsersController {
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signup(body.email, body.password);
     session.userId = user.id;
-    return user; 
+    return user;
   }
 
   @Post('/signin')
