86f15d4f826c057e11ef483d7be24a83c1cbe285
Bit-Barron <1bit.baron@gmail.com>
2023-01-08 17:58:16 +0100

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
+import { CookieSession } from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
