404ee2265a5d53593485e055401a3e4a83ba4fc9
Bit-Barron <1bit.baron@gmail.com>
2023-01-08 21:01:10 +0100

    service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,13 +3,20 @@ import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 
 it('can create an instance of auth service', async () => {
-    const fakeUsersService= {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({id: 1, email, password})
-    }
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
 
-    const module = await Test.createTestingModule({
-    providers: [AuthService],
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
   }).compile();
 
   const service = module.get(AuthService);
