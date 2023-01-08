2124f139f8b8d01836ec4fd6ede8928d817925da
Bit-Barron <1bit.baron@gmail.com>
2023-01-08 20:04:09 +0100

    interceptor

--- src/users/decorators/current-user.decorate.ts
+++ src/users/decorators/current-user.decorate.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { LIMIT_COMPOUND_SELECT } from 'sqlite3';
 
 export const currentUser = createParamDecorator(
   (data: any, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    return 'hi there'
+    return request.currentUser;
   },
 );
---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common/interfaces';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private userService: UsersService) {}
+
+  async intercept(context: ExecutionContext, handler: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session;
+
+    if (userId) {
+      const user = await this.userService.findOne(userId);
+      request.currentUser = user;
+    }
+    return handler.handle();
+  }
+}
