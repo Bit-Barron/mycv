3dc74954294b6f96cb43a434f5530dd83f1544fe
Bit-Barron <1bit.baron@gmail.com>
2023-01-08 18:06:20 +0100

    auth

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
-import { CookieSession } from 'cookie-session';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdfasdf']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
