4c6262e392dc41a0c11f6415585f38b90cde7b1b
Bit-Barron <1bit.baron@gmail.com>
2023-01-09 21:56:02 +0100

    user.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { UsersModule } from './users/users.module';
 import { ReportsController } from './reports/reports.controller';
 import { ReportsModule } from './reports/reports.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { Report } from './reports/report.entity';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
@@ -12,13 +15,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'database.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
     UsersModule,
     ReportsModule,
   ],
