f00e015f22d3101a4da60ade492776c151aaa13a
Bit-Barron <1bit.baron@gmail.com>
2023-01-10 17:51:22 +0100

    reports.module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './report.entity';
+import { ReportsController } from './reports.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report])],
+  imports: [TypeOrmModule.forFeature([Report]), ReportsModule],
   providers: [ReportsService],
+  controllers: [ReportsController],
+
 })
 export class ReportsModule {}
