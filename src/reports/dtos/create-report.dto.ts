8d4001bd55386840c4e93e9b5b225ee2fd3678ed
Bit-Barron <1bit.baron@gmail.com>
2023-01-10 17:09:16 +0100

    finished req body dto

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,19 +1,35 @@
 import {
-    IsString,
-    isNumber,
-    Min,
-    Max,
-    IsLongitude,
-    IsLatitude
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
 } from 'class-validator';
 
-
 export class CreateReportDto {
-    make: string;
-    model: string;
-    year: number;
-    mileage: number;
-    lng: number;
-    lat: number;
-    price: number;
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  @Min(0)
+  @Max(1000000)
+  lat: number;
+  price: number;
 }
