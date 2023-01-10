530bface4db87116869d79ac3087172a3e6372c1
Bit-Barron <1bit.baron@gmail.com>
2023-01-10 20:42:34 +0100

    controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,14 +2,15 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
-
+import { currentUser } from 'src/users/decorators/current-user.decorate';
+import { User } from 'src/users/user.entity';
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
+  createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
     return this.reportsService.create(body);
   }
 }
