41936640723efa4575e377f0d24ef9e72ef9f500
Bit-Barron <1bit.baron@gmail.com>
2023-01-10 17:45:30 +0100

    reports

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './report.entity';
-import { ReportsController } from './reports.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Report])],
