9b9ce45888c4f87be1de1519f1a6aeb7b9e218e9
Bit-Barron <1bit.baron@gmail.com>
2023-01-10 21:28:01 +0100

    controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { currentUser } from 'src/users/decorators/current-user.decorate';
 import { User } from 'src/users/user.entity';
-import { ReportDto } from './dtos/report.dto';
+import { ApprovedReportsDto } from './dtos/approved-report.dto';
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
@@ -14,4 +21,15 @@ export class ReportsController {
   createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
     return this.reportsService.create(body, user);
   }
+
+  @Patch('/:id')
+  approvedReport(@Param('id') id: string, @Body() body: ApprovedReportsDto) {}
 }
+
+
+// async changeApproval(id: string, approved: boolean) {
+//   const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+
+//   ...
+
+// }
